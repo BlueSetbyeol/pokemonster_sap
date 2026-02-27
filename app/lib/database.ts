7075c3321940d82a7c45b34cd6660b83f27a4549
BlueSetbyeol <setbyeol.c@gmail.com>
2026-02-27 20:38:30 +0100

    debug2

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -2,21 +2,6 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log(
-  "POSTGRES_URL",
-  process.env.POSTGRES_URL,
-  "POSTGRES_USER",
-  process.env.POSTGRES_USER,
-  "POSTGRES_HOST",
-  process.env.POSTGRES_HOST,
-  "POSTGRES_DATABASE",
-  process.env.POSTGRES_DATABASE,
-  "POSTGRES_PASSWORD",
-  process.env.POSTGRES_PASSWORD,
-  "PORT",
-  process.env.PORT,
-);
-
 const connectionPool = new Pool({
   connectionString: process.env.POSTGRES_URL,
   user: process.env.POSTGRES_USER,
